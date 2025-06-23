d711b12bffe289549ee2995016bc14c1c03ba1c5
rphii <raphael.seitz18@gmail.com>
2025-06-23 22:44:39 +0200

    now we don't print it twicegit statusgit status

--- src/main.c
+++ src/main.c
@@ -5,12 +5,66 @@
 #include <rphii/arg.h>
 #include <time.h>
 #include <pwd.h>
+#include <sys/ioctl.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <rphii/file.h>
 
-typedef struct PS1Config {
-    bool nocolor;
-} PS1Config;
+#include <ps1-config.h>
+
+
+int get_pos(int *x, int *y) {
+
+    char buf[30]={0};
+    int ret, i, pow;
+    char ch;
+
+    *y = 0; *x = 0;
+
+    struct termios term, restore;
+
+    tcgetattr(0, &term);
+    tcgetattr(0, &restore);
+    term.c_lflag &= ~(ICANON|ECHO);
+    tcsetattr(0, TCSANOW, &term);
+
+    write(1, "\033[6n", 4);
+
+    for( i = 0, ch = 0; ch != 'R'; i++ )
+    {
+        ret = read(0, &ch, 1);
+        if ( !ret ) {
+            tcsetattr(0, TCSANOW, &restore);
+            fprintf(stderr, "getpos: error reading response!\n");
+            return 1;
+        }
+        buf[i] = ch;
+        //printf("buf[%d]: \t%c \t%d\n", i, ch, ch);
+    }
+
+    if (i < 2) {
+        tcsetattr(0, TCSANOW, &restore);
+        //printf("i < 2\n");
+        return(1);
+    }
+
+    for( i -= 2, pow = 1; buf[i] != ';'; i--, pow *= 10)
+        *x = *x + ( buf[i] - '0' ) * pow;
+
+    for( i-- , pow = 1; buf[i] != '['; i--, pow *= 10)
+        *y = *y + ( buf[i] - '0' ) * pow;
+
+    tcsetattr(0, TCSANOW, &restore);
+    return 0;
+}
 
 int main(const int argc, const char **argv) {
+    
+    PS1Config preset = {
+        .fmt_time.fg = {{ 0x767676ff }},
+    };
 
     int err = 0;
     PS1Config config = {0};
@@ -28,6 +82,16 @@ int main(const int argc, const char **argv) {
       argx_help(x, arg);
     x=argx_init(arg_opt(arg), 'C', str("nocolor"), str("output without color"));
       argx_bool(x, &config.nocolor, 0);
+    x=argx_init(arg_opt(arg), 0, str("fmt-time-fg"), str("color of time foreground"));
+      argx_col(x, &config.fmt_time.fg, &preset.fmt_time.fg);
+    x=argx_init(arg_opt(arg), 0, str("fmt-time-bg"), str("color of time background"));
+      argx_col(x, &config.fmt_time.bg, &preset.fmt_time.bg);
+    x=argx_init(arg_opt(arg), 0, str("fmt-time-bold"), str("time bold"));
+      argx_bool(x, &config.fmt_time.bold, &preset.fmt_time.bold);
+    x=argx_init(arg_opt(arg), 0, str("fmt-time-italic"), str("time italic"));
+      argx_bool(x, &config.fmt_time.italic, &preset.fmt_time.italic);
+    x=argx_init(arg_opt(arg), 0, str("fmt-time-underline"), str("time underline"));
+      argx_bool(x, &config.fmt_time.underline, &preset.fmt_time.underline);
 
     TRYC(arg_parse(arg, argc, argv, &exit_early));
     if(exit_early) goto clean;
@@ -42,14 +106,16 @@ int main(const int argc, const char **argv) {
     Str home = str_l(secure_getenv("HOME"));
     Str cwd = str_l(ccwd);
 
-    Color col_time = {{ 0x767676ff }};
+    //Color col_time = {{ 0x767676ff }};
     Color col_user = {{ 0xd3777dff }};
     Color col_path = {{ 0x87af87ff }};
     Color col_icon = {{ 0xffff00ff }};
 
-    StrFmtX x_time = config.nocolor ? (StrFmtX){0} : (StrFmtX){ .fg = col_time };
-    StrFmtX x_user = config.nocolor ? (StrFmtX){0} : (StrFmtX){ .fg = col_user };
-    StrFmtX x_icon = config.nocolor ? (StrFmtX){0} : (StrFmtX){ .fg = col_icon };
+    StrFmtX x_time = config.fmt_time;
+    x_time.nocolor = &config.nocolor;
+    x_time.bashsafe = true;
+    StrFmtX x_user = config.nocolor ? (StrFmtX){0} : (StrFmtX){ .bashsafe = true, .fg = col_user };
+    StrFmtX x_icon = config.nocolor ? (StrFmtX){0} : (StrFmtX){ .bashsafe = true, .fg = col_icon };
     
     time_t rawtime;
     time(&rawtime);
@@ -89,14 +155,14 @@ int main(const int argc, const char **argv) {
         bool first = !(splice.str > path.str);
         //if(last) col_path.rgba = 0x11ff11ff;
         char *folder = ((single && *splice.str == '/') || !first) ? "/" : "";
-        str_fmtx(&out, config.nocolor ? (StrFmtX){0} : (StrFmtX){ .fg = col_path, .bold = single }, "%s", folder);
+        str_fmtx(&out, config.nocolor ? (StrFmtX){0} : (StrFmtX){ .bashsafe = true, .fg = col_path, .bold = single }, "%s", folder);
         if(last && !single) col_path.rgba = 0xffffffff;
-        str_fmtx(&out, config.nocolor ? (StrFmtX){0} : (StrFmtX){ .fg = col_path, .bold = last }, "%.*s", STR_F(splice));
+        str_fmtx(&out, config.nocolor ? (StrFmtX){0} : (StrFmtX){  .bashsafe = true,.fg = col_path, .bold = last }, "%.*s", STR_F(splice));
         col_path.r += 10;
         col_path.g += 10;
         col_path.b += 10;
     }
-    //str_push(&out, ' ');
+    str_push(&out, ' ');
 
     str_print(out);
 
---
+++ src/ps1-config.h
@@ -0,0 +1,18 @@
+#ifndef PS1_CONFIG_H
+
+#include <stdbool.h>
+#include <rphii/color.h>
+#include <rphii/str.h>
+
+typedef struct PS1Config {
+    bool nocolor;
+    bool fixspacing;
+    StrFmtX fmt_time;
+    StrFmtX fmt_user;
+    StrFmtX fmt_path;
+    Color col_path;
+} PS1Config;
+
+#define PS1_CONFIG_H
+#endif
+
